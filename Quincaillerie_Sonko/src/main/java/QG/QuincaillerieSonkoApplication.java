667b1654b1c6cc281ab4abe2145cd51ba090ca9e
Sindidi Job <sindididiop@gmail.com>
2021-10-11 19:19:20 +0000

    first commit

---
+++ Quincaillerie_Sonko/src/main/java/QG/QuincaillerieSonkoApplication.java
@@ -0,0 +1,13 @@
+package QG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuincaillerieSonkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuincaillerieSonkoApplication.class, args);
+	}
+
+}
---
+++ Quincaillerie_Sonko/src/test/java/QG/QuincaillerieSonkoApplicationTests.java
@@ -0,0 +1,13 @@
+package QG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuincaillerieSonkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
